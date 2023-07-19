3d3cc220282e66cd08552250d5506c23f19a837b
SS08090 <techicalsolutions68@gmail.com>
2023-07-19 22:35:21 +0530

    upload and delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +9,31 @@ void main() {
 
 List<String> feedItems = [];
 List<String> feedId = [];
+
+class CustomFloatingActionButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+  final IconData icon;
+
+  const CustomFloatingActionButton({
+    required this.onPressed,
+    required this.backgroundColor,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      shape: CircleBorder(),
+      elevation: 4.0,
+      child: FloatingActionButton(
+        onPressed: onPressed,
+        backgroundColor: backgroundColor,
+        child: Icon(icon),
+      ),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -32,13 +58,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var driveurl;
+  String serverurl = 'original-google.onrender.com';
   Color fabColor = Colors.blue;
-
+  Timer? periodicTimer; // Timer instance
   Map<String, bool> buttonStatusMap = {};
 
   void getHttp() async {
     try {
-      var uri = Uri.https('original-google.onrender.com', '/getfiles');
+      var uri = Uri.https(serverurl, '/getfiles');
       http.Response response = await http.get(uri);
       if (response.statusCode == 200) {
         debugPrint(response.body.toString());
@@ -47,14 +74,15 @@ class _MyHomePageState extends State<MyHomePage> {
           if (item['mimeType'] != 'application/vnd.google-apps.folder') {
             String name = item['name'];
             String id = item['id'];
-            feedItems.add('$name');/*$name - $id*/
+            feedItems.add('$name'); /*$name - $id*/
             feedId.add('$id');
           }
         }
         print(feedItems);
         setState(() {
           driveurl = response.body.toString();
-          buttonStatusMap = Map.fromIterable(feedId, key: (id) => id, value: (_) => true);
+          buttonStatusMap =
+              Map.fromIterable(feedId, key: (id) => id, value: (_) => true);
         });
       } else {
         print('Request failed with status: ${response.statusCode}');
@@ -64,35 +92,60 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  void getHttp2() async {
-    try {
-      final response = await http.get(Uri.parse('https://original-google.onrender.com'));
-      if (response.statusCode == 200) {
-        setState(() {
-          fabColor = Colors.green;
-        });
-      } else {
+  void getHttp2() {
+    void fetchStatus() async {
+      try {
+        final response =
+            await http.get(Uri.parse('https://'+serverurl));
+        if (response.statusCode == 200) {
+          setState(() {
+            fabColor = Colors.green;
+          });
+        } else {
+          setState(() {
+            fabColor = Colors.red;
+          });
+        }
+      } catch (e) {
         setState(() {
           fabColor = Colors.red;
         });
       }
-    } catch (e) {
-      setState(() {
-        fabColor = Colors.red;
-      });
     }
+
+    periodicTimer = Timer.periodic(Duration(seconds: 30), (_) {
+      fetchStatus();
+      print('periodicTimer');
+    });
   }
 
   void getHttp3(var para) async {
     try {
-      var uri = Uri.https('original-google.onrender.com', '/createrepo', {
+      var uri = Uri.https(serverurl, '/createrepo', {
         'fileid': para,
       });
       http.Response response = await http.get(uri);
       if (response.statusCode == 200) {
         var jsonResponse = json.decode(response.body);
+      } else {
+        print('Request failed with status: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  void getHttp4(var para) async {
+    try {
+      var uri = Uri.https(serverurl, '/deletefile', {
+        'file_id': para,
+      });
+      http.Response response = await http.get(uri);
+      if (response.statusCode == 200) {
+        var jsonResponse = json.decode(response.body);
+        // If the deletion was successful, refresh the widget by calling getHttp()
         setState(() {
-          buttonStatusMap[para] = jsonResponse != null && jsonResponse.isNotEmpty;
+          buttonStatusMap[para] = false; // Assuming the item was deleted successfully
         });
       } else {
         print('Request failed with status: ${response.statusCode}');
@@ -112,17 +165,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Column(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    getHttp();
-                    print('ElevatedButton pressed');
-                  },
-                  child: Text('Fetch Files'),
-                ),
-              ],
-            ),
             Expanded(
               child: ListView.builder(
                 itemCount: feedItems.length,
@@ -130,19 +172,40 @@ class _MyHomePageState extends State<MyHomePage> {
                   String id = feedId[index];
                   bool isButtonEnabled = buttonStatusMap[id] ?? true;
                   return Card(
-                    child: ListTile(
-                      title: Text(feedItems[index]),
-                      trailing: ElevatedButton(
-                        onPressed: isButtonEnabled
-                            ? () {
-                          getHttp3(id);
-                        }
-                            : null,
-                        style: ElevatedButton.styleFrom(
-                          primary: isButtonEnabled ? Colors.blue : Colors.grey,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text(
+                            feedItems[index],
+                          ),
                         ),
-                        child: Text('Upload'),/*$index*/
-                      ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              onPressed: isButtonEnabled
+                                  ? () {
+                                      getHttp3(id);
+                                    }
+                                  : null,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    isButtonEnabled ? Colors.blue : Colors.grey,
+                              ),
+                              child: Text('Upload'), /*$index*/
+                            ),
+                            ElevatedButton(
+                              onPressed:
+                                  isButtonEnabled ? () => getHttp4(id) : null,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    isButtonEnabled ? Colors.red : Colors.grey,
+                              ),
+                              child: Text('Remove'),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   );
                 },
@@ -151,11 +214,21 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getHttp2,
-        tooltip: 'Increment',
-        backgroundColor: fabColor,
-        child: const Icon(Icons.power),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CustomFloatingActionButton(
+            onPressed: getHttp2,
+            backgroundColor: fabColor,
+            icon: Icons.power,
+          ),
+          SizedBox(height: 12), // Adjust the spacing between the FABs
+          CustomFloatingActionButton(
+            onPressed: getHttp,
+            backgroundColor: Colors.orange, // Set the desired background color
+            icon: Icons.add, // Set the desired icon
+          ),
+        ],
       ),
     );
   }
